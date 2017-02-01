88dc9323548f7883c71c9b556000b968c44ef6a9
suhasspai <suhasspai@gmail.com>
2017-02-01 19:57:58 +0530

    New Template

--- Website/js/position.js
+++
@@ -1,9 +0,0 @@
-function stickyScroll(e) {
-	if(window.pageYOffset > 400) {
-		document.getElementById('navig').className += 'navbar-fixed-top';
-	}
-	if(window.pageYOffset < 400) {
-		document.getElementById('navig').className.replace(/(?:^|\s)navbar-fixed-top(?!\S)/g, '');
-	}
-}
-window.addEventListener('scroll', stickyScroll, false);
